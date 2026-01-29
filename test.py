095a5e16ca191b64ac856e47ec3968a1ed6862ca
vaibhavposture <97782006+vaibhavposture@users.noreply.github.com>
2026-01-29 13:31:12 +0530

    update2 test.py
    added multiplication

--- test.py
+++ test.py
@@ -1,5 +1,9 @@
 def add(x,y):
     z =x+y
     return z
+def mult(x,y):
+    z =x+y
+    return z
 sum = add(10,7)
-print(f'Hello Vaibhav Sum is {sum}')
+mult = mult(2,8)
+print(f'Hello Vaibhav Sum is {sum} & Multiplication is {mult}')
